9a8ad41ccb1e3dcb71e8fb4bf896daa20d3a7dbb
anshulv1401 <anshulv1401@gmail.com>
2023-09-06 23:40:40 +0530

    10-arrays-exercies done

--- DSA/CustomArray.cs
+++ DSA/CustomArray.cs
@@ -17,14 +17,7 @@
 
         public void Insert(int item)
         {
-            if (array.Length == lastIndex + 1)
-            {
-                var newArray = new int[array.Length * 2];
-                for (int i = 0; i < array.Length; i++)
-                    newArray[i] = array[i];
-                array = newArray;
-            }
-            lastIndex++;
+            AutoIncrementArrayIfRequired();
             array[lastIndex] = item;
         }
 
@@ -35,7 +28,7 @@
                 throw new ArgumentOutOfRangeException("index");
             }
 
-            for (int i = index; i < array.Length - 1; i++)
+            for (int i = index; i < lastIndex; i++)
                 array[i] = array[i + 1];
 
             lastIndex--;
@@ -44,6 +37,75 @@
         public int IndexOf(int item)
         {
             return Array.IndexOf(array, item);
+        }
+
+        public int Max()
+        {
+            int max = int.MinValue;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                    max = array[i];
+            }
+            return max;
+        }
+
+        public int[] Intersect(int[] array)
+        {
+            var commonArray = new CustomArray(array.Length);
+            for (int i = 0; i < this.array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (this.array[i] == array[j])
+                    {
+                        commonArray.Insert(array[j]);
+                    }
+                }
+            }
+            return commonArray.ToArray();
+        }
+
+        public void ReverseArray()
+        {
+            for (int i = 0; i < ((lastIndex + 1) / 2); i++)
+            {
+                (array[lastIndex - i], array[i]) = (array[i], array[lastIndex - i]);
+            }
+        }
+
+        public void InsertAt(int index, int item)
+        {
+            if (index < 0 || index > lastIndex)
+            {
+                throw new ArgumentOutOfRangeException("index");
+            }
+
+            AutoIncrementArrayIfRequired();
+
+            for (int i = lastIndex; i >= index; i--)
+                array[i + 1] = array[i];
+            array[index] = item;
+        }
+
+        private void AutoIncrementArrayIfRequired()
+        {
+            if (array.Length == lastIndex + 1)
+            {
+                var newArray = new int[array.Length * 2];
+                for (int i = 0; i < array.Length; i++)
+                    newArray[i] = array[i];
+                array = newArray;
+            }
+            lastIndex++;
+        }
+
+        public int[] ToArray()
+        {
+            int[] newArray = new int[lastIndex + 1];
+            for (int i = 0; i <= lastIndex; i++)
+                newArray[i] = array[i];
+            return newArray;
         }
     }
 }
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -7,7 +7,25 @@ customArray.Insert(10);
 customArray.Insert(20);
 customArray.Insert(30);
 customArray.Insert(40);
+customArray.Insert(-10000000);
+customArray.Insert(1000000);
+customArray.Insert(56);
 customArray.RemoveAt(1);
+Console.WriteLine("Max is " + customArray.Max());
 customArray.Print();
-Console.WriteLine(customArray.IndexOf(30));
-Console.WriteLine(customArray.IndexOf(100));
+
+var intersect = customArray.Intersect(new int[] { 10, 10, 10, 10, 10, 10, 20, 40, 30 });
+
+Console.WriteLine("Intersect is:");
+foreach (var item in intersect)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+// customArray.ReverseArray();
+// Console.WriteLine("Reverse is:");
+// customArray.Print();
+
+customArray.InsertAt(1, 20);
+Console.WriteLine("After inserting 20 at index 1:");
+customArray.Print();
