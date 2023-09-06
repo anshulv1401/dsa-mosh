57f746df7a2510120f4035f351db577cc92e54db
anshulv1401 <anshulv1401@gmail.com>
2023-09-06 22:44:21 +0530

    Arrays

---
+++ DSA/CustomArray.cs
@@ -0,0 +1,49 @@
+﻿namespace DSA
+{
+    public class CustomArray
+    {
+        private int[] array;
+        private int lastIndex = -1;
+        public CustomArray(int size)
+        {
+            array = new int[size];
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i <= lastIndex; i++)
+                Console.WriteLine(array[i]);
+        }
+
+        public void Insert(int item)
+        {
+            if (array.Length == lastIndex + 1)
+            {
+                var newArray = new int[array.Length * 2];
+                for (int i = 0; i < array.Length; i++)
+                    newArray[i] = array[i];
+                array = newArray;
+            }
+            lastIndex++;
+            array[lastIndex] = item;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index > lastIndex)
+            {
+                throw new ArgumentOutOfRangeException("index");
+            }
+
+            for (int i = index; i < array.Length - 1; i++)
+                array[i] = array[i + 1];
+
+            lastIndex--;
+        }
+
+        public int IndexOf(int item)
+        {
+            return Array.IndexOf(array, item);
+        }
+    }
+}
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DSA;
+
+var customArray = new CustomArray(3);
+
+customArray.Insert(10);
+customArray.Insert(20);
+customArray.Insert(30);
+customArray.Insert(40);
+customArray.RemoveAt(1);
+customArray.Print();
+Console.WriteLine(customArray.IndexOf(30));
+Console.WriteLine(customArray.IndexOf(100));
