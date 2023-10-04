62687fcd6148620911c4ebe75a75a83593545474
Anshul Vanawat <anshulv1401@gmail.com>
2023-10-05 00:43:50 +0530

    CheckForTarget and twoSums

--- DSA/LeetCode/ArraysAndString/ArraysAndStrings.cs
+++ DSA/LeetCode/ArraysAndString/ArraysAndStrings.cs
@@ -16,5 +16,46 @@ namespace DSA.LeetCode.ArraysAndString
             }
             return true;
         }
+
+        //nums is sorted
+        //Given a sorted array of unique integers and a target integer, return true if there exists a pair of numbers that sum to target, false otherwise.
+        public static bool CheckForTarget(int[] nums, int target)
+        {
+
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int currentSum = nums[left] + nums[right];
+                if (currentSum == target)
+                    return true;
+                if (currentSum < target)
+                    left++;
+                else
+                    right--;
+            }
+
+            return false;
+        }
+
+        //nums is unsorted
+        //https://leetcode.com/problems/two-sum/
+        //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var diff = target - nums[i];
+                if (dict.ContainsKey(diff) && dict[diff] != i)
+                {
+                    return new int[] { dict[diff], i };
+                }
+                dict.TryAdd(nums[i], i);
+            }
+            return Array.Empty<int>();
+        }
     }
 }
--- DSA/LeetCode/Program.cs
+++ DSA/LeetCode/Program.cs
@@ -5,5 +5,8 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine(ArraysAndStrings.IsPalindrome("racecar"));
+        Console.WriteLine(ArraysAndStrings.CheckForTarget(new int[] { 10 }, 100));
+        Console.WriteLine(string.Join(",", ArraysAndStrings.TwoSum(new int[] { 2, 7, 11, 15 }, 9)));
+        Console.WriteLine(string.Join(",", ArraysAndStrings.TwoSum(new int[] { 3, 6, 7, 8, 9, 1, 6, 7, 8, 3 }, 6)));
     }
 }
