fe4d61220fa9b96b92b1efc3a1d6667dbfadfed6
Anshul Vanawat <anshulv1401@gmail.com>
2023-10-05 00:05:38 +0530

    SmallestSubstringPattern

--- DSA/Part3/PracticeTest.cs
+++ DSA/Part3/PracticeTest.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace DSA.Part3
 {
     class PracticeTest
@@ -42,6 +44,77 @@ namespace DSA.Part3
                 }
             }
             return sum;
+        }
+
+        public static string SmallestSubstringPattern(string input, string ptrn)
+        {
+            if (input.Length < ptrn.Length)
+            {
+                return "";
+            }
+
+            Dictionary<char, int> patternDict = new();
+
+            foreach (var ch in ptrn)
+            {
+                if (patternDict.ContainsKey(ch))
+                    patternDict[ch]++;
+                else
+                    patternDict.Add(ch, 1);
+            }
+
+            Dictionary<char, int> strDict = new();
+
+            int start = 0, start_index = -1, min_len = int.MaxValue;
+            int count = 0;
+
+            for (int currentIndex = 0; currentIndex < input.Length; currentIndex++)
+            {
+                var currentCh = input[currentIndex];
+
+                if (patternDict.ContainsKey(currentCh))
+                {
+                    if (strDict.ContainsKey(currentCh))
+                        strDict[currentCh]++;
+                    else
+                        strDict.Add(currentCh, 1);
+
+                    if (strDict[currentCh] <= patternDict[currentCh])
+                        count++;
+                }
+
+                if (count == ptrn.Length)
+                {
+                    while (true && start < input.Length)
+                    {
+                        var startingChar = input[start];
+
+                        if (patternDict.ContainsKey(startingChar))
+                        {
+                            strDict[startingChar]--;
+                            if (patternDict[startingChar] > strDict[startingChar])
+                            {
+                                count--;
+                                break;
+                            }
+                        }
+                        start++;
+                    }
+
+                    var currentWindowLength = currentIndex - start + 1;
+                    if (min_len > currentWindowLength)
+                    {
+                        min_len = currentWindowLength;
+                        start_index = start;
+                    }
+                    start++;
+                }
+            }
+
+            if (start_index == -1)
+                return "";
+            else
+                return input.Substring(start_index, min_len);
         }
     }
 }
--- DSA/Part3/Program.cs
+++ DSA/Part3/Program.cs
@@ -78,8 +78,18 @@
 
 using DSA.Part3;
 
-string[] ops = new string[] { "5", "2", "D", "C", "+" };
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] ops = new string[] { "5", "2", "D", "C", "+" };
 
-int output = PracticeTest.CalPoints(ops);
+        int output = PracticeTest.CalPoints(ops);
 
-Console.Write(output.ToString());
+        Console.WriteLine(output.ToString());
+
+
+        Console.WriteLine(PracticeTest.SmallestSubstringPattern("this is a test string", "tist")); ;
+        Console.WriteLine(PracticeTest.SmallestSubstringPattern("geeksforgeeks", "ksfor"));
+    }
+}
